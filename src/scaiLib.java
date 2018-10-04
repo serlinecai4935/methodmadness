1a097543d95832c8b1457de8bd6eb73af15ae4e0
BT_1N3_21 <scai4935@bths.edu>
2018-10-04 14:48:19 -0400

    first method

---
+++ src/scaiLib.java
@@ -0,0 +1,16 @@
+public class scaiLib
+    //String Methods: dateStr
+    public static String dateStr(String Name){
+      String date = str.substring(3,5);
+        return date;
+      String month = str.substring(0,2);
+        return month;
+      String year = str.substring(6,10);
+        return year;
+
+      system.println (date+"-"+month+"-"+year)
+
+    }
+}
+
+//
